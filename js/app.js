5ee4a91b9566bd7b4e1e5a74929ff0d9ddf1372b
lotusgraham <gwhitley99@gmail.com>
2016-02-19 15:32:58 -0500

    update clear button, update strike-through, update css layout styles

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 $(document).ready(function () {
-  //get items input
-  $('#addstuffbutton').click(function(){
 
-  var userInput = $('#addstuff').val();
-  console.log(userInput);
+  $('#listofstuff').submit(function(e){
+    e.preventDefault();
+      var userInput = $('#addstuff').val();
+      console.log(userInput);
 
-  $('#newitem').append('<li class="items">'+userInput+'</li>');
-  $('#addstuff').val('');
+      $('#newitem').append('<li class="items">'+userInput+'</li>');
+      $('#addstuff').val('');
+    });
+
+    $('#clearbutton').click(function(){
+      // alert('testing');
+      $('#newitem').empty();
+    });
+
+      $('#addstuff').on ('click', function(){
+         $('#addstuff').val(' ');
+    });
+      $('#newitem').on('click', 'li.items', function() {
+        $(this).wrap("<strike>");
+      });
+
+});
+
+
+//get items input
+//
+// $('#clearbutton').click(function(){
+// $('#newitem').clear();
 
 //clear the data within the addstuff button
 //make .keydown enter #addstuff to the list
 
   // alert(userInput);
-
-
-});
-
-})
